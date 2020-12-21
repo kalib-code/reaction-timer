8ab5b8f8f7a8440ff8d1d9dfc7fe4bab02b01d23
Kalib <kalibuas@gmail.com>
2020-12-21 20:45:26 +0800

    master

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,23 @@
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame" />
-  <p> Reaction time: {{ score }} ms</p>
+  <Result v-if="showResults" :score="score"/>
+  
 </template>
 
 <script>
 import Block from "./components/Block.vue";
+import Result from "./components/Result.vue";
 
 export default {
   name: "App",
-  components: { Block },
+  components: { Block, Result },
   data() {
     return {
       isPlaying: false,
       delay: null,
       score: null,
+      showResults:false,
     };
   },
   methods: {
@@ -26,6 +29,7 @@ export default {
     endGame(reactionTime) {
       this.score = reactionTime;
       this.isPlaying = false;
+      this.showResults = true;
     },
   },
 };
